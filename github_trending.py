72748734555b778eb87e86ae1f3709c69ac61d4e
kporcelain <zhukovaksusha@gmail.com>
2017-12-07 17:06:28 +0300

    added links to repos

--- github_trending.py
+++ github_trending.py
@@ -10,7 +10,7 @@ def get_trending_repositories():
 def get_open_issues_amount(trending_repos):
     dictionary_of_names_and_issues = {}
     for feature in trending_repos['items']:
-        dictionary_of_names_and_issues[feature["name"]] = feature['open_issues_count']
+        dictionary_of_names_and_issues[feature["name"]] = [feature['open_issues_count'], feature['svn_url']]
     for repo_name in sorted(dictionary_of_names_and_issues, key=lambda capital_letter: capital_letter.upper()):
         repo_issues = dictionary_of_names_and_issues[repo_name]
         print(repo_name, "-", repo_issues)
