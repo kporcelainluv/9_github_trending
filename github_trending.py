54c3b8d9e714f9be76e8c64688cb371193d3dd75
Ksusha Zhukova <zhukovaksusha@gmail.com>
2017-12-28 15:00:27 +0300

    updated var name, used params

--- github_trending.py
+++ github_trending.py
@@ -3,23 +3,25 @@ import datetime
 
 
 def get_trending_repositories():
-    trending_repos = requests.get(url, params=set_of_settings)
+    today = datetime.date.today()
+    week_ago = today - datetime.timedelta(days=7)
+    number_of_repos = 20
+    params = {'q': 'created:>={}'.format(week_ago.isoformat()), "sort": "stars", "order": "desc", "page": "1",
+              "per_page": number_of_repos}
+    trending_repos = requests.get("https://api.github.com/search/repositories", params=params)
     return trending_repos.json()
 
 
-def get_open_issues_amount(trending_repos):
-    dictionary_of_names_and_issues = {}
-    for feature in trending_repos['items']:
-        dictionary_of_names_and_issues[feature["name"]] = [feature['open_issues_count'], feature['svn_url']]
-    for repo_name in sorted(dictionary_of_names_and_issues, key=lambda capital_letter: capital_letter.upper()):
-        repo_issues = dictionary_of_names_and_issues[repo_name]
-        print(repo_name, "-", repo_issues)
+def get_open_issues_amount(link):
+    issues_url = '{0}/issues'.format(link)
+    return len(requests.get(issues_url).json())
 
 
 if __name__ == '__main__':
-    today = datetime.date.today()
-    week_ago = today - datetime.timedelta(days=7)
-    url = 'https://api.github.com/search/repositories'
-    #помогите, пожалуйста, с 'created' на след строчке! должно получиться "created:>=date", у меня никак не выходит
-    set_of_settings = (('created:>{}'.format(str(week_ago)), str(week_ago)), ("sort", "stars"), ("order", "desc"), ("page", "1"), ("per_page", "20"))
-    print(get_open_issues_amount(get_trending_repositories()))
+    for feature in get_trending_repositories()["items"]:
+        repo_name = feature['name']
+        user_name = feature['owner']['login']
+        link = feature['url']
+        print("User name - ", user_name, end="; ")
+        print("Repository name - ", repo_name, end="; ")
+        print("Number of issues - ", get_open_issues_amount(link))
