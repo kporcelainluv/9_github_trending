16a4383962f6fec4ac2846d3e70e4df3a3810e89
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-01-06 21:51:04 +0300

    updated var names

--- github_trending.py
+++ github_trending.py
@@ -3,12 +3,16 @@ import datetime
 
 
 def get_trending_repositories():
+    repos_api_link = "https://api.github.com/search/repositories"
     today = datetime.date.today()
     week_ago = today - datetime.timedelta(days=7)
     number_of_repos = 20
-    params = {'q': 'created:>={}'.format(week_ago.isoformat()), "sort": "stars", "order": "desc", "page": "1",
+    params = {'q': 'created:>={}'.format(week_ago.isoformat()),
+              "sort": "stars",
+              "order": "desc",
+              "page": "1",
               "per_page": number_of_repos}
-    trending_repos = requests.get("https://api.github.com/search/repositories", params=params)
+    trending_repos = requests.get(api_link, params=params)
     return trending_repos.json()
 
 
@@ -18,10 +22,10 @@ def get_open_issues_amount(link):
 
 
 if __name__ == '__main__':
-    for feature in get_trending_repositories()["items"]:
-        repo_name = feature['name']
-        user_name = feature['owner']['login']
-        link = feature['url']
+    for repo in get_trending_repositories()["items"]:
+        repo_name = repo['name']
+        user_name = repo['owner']['login']
+        link = repo['url']
         print("User name - ", user_name, end="; ")
         print("Repository name - ", repo_name, end="; ")
         print("Number of issues - ", get_open_issues_amount(link))
